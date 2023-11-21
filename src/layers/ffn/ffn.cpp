4d2865be52271c6d77355bc8c5c75f45a3834192
Zhiwei35 <1741877821@qq.com>
2023-11-20 21:32:06 -0800

    Impl ffn layer of LLaMA

---
+++ src/layers/ffn/ffn.cpp
@@ -0,0 +1,54 @@
+#include <math.h>
+#include "src/layers/ffn/ffn.h"
+LLaMAFFNLayer::LLaMAFFNLayer(int head_num,
+                               int head_size,
+                               int inter_size,
+                               cudaStream_t stream,
+                               cublasWrapper* cublas_wrapper,
+                               BaseAllocator* allocator,
+                               bool is_free_buffer_after_fwd):
+    head_num(head_num),
+    head_size(head_size),
+    inter_size(inter_size),
+    stream(stream),
+    cublas_wrapper(cublas_wrapper),
+    allocator(allocator), //cudaAllocator
+    hidden_units(head_num * head_size),
+    is_free_buffer_after_fwd(is_free_buffer_after_fwd) {}
+
+template<typename T>
+LLaMAFFNLayer::allocForForward(LLaMAAttentionDynParams& params){
+    int num_tokens = params.num_tokens;
+    DataType type = getTensorType<T>(); 
+    SwiGLU_input = new Tensor(Device::GPU, type, {2, num_tokens, inter_size});
+    down_proj_input = new Tensor(Device::GPU, type, {num_tokens, inter_size});
+    down_proj_output = new Tensor(Device::GPU, type, {num_tokens, hidden_units});
+    SwiGLU_input->data = allocator->Malloc(SwiGLU_input->data, sizeof(T) * num_tokens * 2 * inter_size);
+    down_proj_input->data = allocator->Malloc(down_proj_input->data, sizeof(T) * num_tokens * inter_size);
+    down_proj_output->data = allocator->Malloc(down_proj_output->data, sizeof(T) * num_tokens * hidden_units);
+}
+
+void LLaMAContextAttentionLayer::free(){
+    allocator->free((void**)(&SwiGLU_input->data));
+    allocator->free((void**)(&down_proj_input->data));
+    allocator->free((void**)(&down_proj_output->data));
+}
+
+LLaMAFFNLayer::forward(TensorMap& inputs, TensorMap& outputs, LLaMAFFNWeights& weights, LLaMAAttentionDynParams& params){
+    allocForForward<float>(params);
+
+    Tensor ffn_input = inputs["ffn_input"];
+    Tensor ffn_output = outputs["ffn_output"];
+    // gate proj
+    launchLinearGemm(&ffn_input, weights.gate, &SwiGLU_input);
+    // up proj
+    launchLinearGemm(&ffn_input, weights.up, &SwiGLU_input, false, false, true);
+
+    launchAct(&SwiGLU_input, &down_proj_input);// down_proj_input maybe can reuse swiglu_input buf, will validate it later
+    //down proj
+    launchLinearGemm(&down_proj_input, weights.down, &down_proj_output);
+
+    if (is_free_buffer_after_fwd) {
+        this->free();
+    }
+}
---
+++ src/layers/ffn/ffn.h
@@ -0,0 +1,48 @@
+#include "src/weights/llama/attention_weights.h"
+#include "src/memory/allocator/cuda_allocator.h"
+#include "src/kernels/qkv_linear.h"
+#include "src/utils/tensor.h"
+#include "src/kernels/cublas_wrapper.h"
+#include "src/models/llama/llama_params.h"
+#include "src/kernels/activation_kernel.h"
+
+template<typename T>
+class LLaMAFFNLayer {
+private:
+    // this params are shared across all LLMs
+    const int head_num;
+    const int head_size;
+    const int inter_size;
+    const int hidden_units;
+    const bool is_free_buffer_after_fwd;
+    const bool is_1st_epoch; // judge if its 1st epoch, if so, we will allocate kv cache
+    // this params are dynamic
+    const LLaMAAttentionDynParams attn_dyn_params;
+
+    cudaStream_t stream;
+    BaseAllocator* allocator;
+    // for linear proj
+    cublasWrapper* cublas_wrapper;
+
+    // buffer
+    // [num tokens, 2, intersize]
+    Tensor*  SwiGLU_input = nullptr;  //gate proj and up proj output buf   
+    // [num tokens, intersize] 
+    Tensor*  down_proj_input = nullptr; 
+    // [num tokens, hiddenunits]
+    Tensor*  down_proj_output = nullptr;
+  
+
+
+public:
+    LLaMAFFNLayer(int head_num,
+                    int head_size,
+                    cudaStream_t stream,
+                    cublasWrapper* cublas_wrapper,
+                    BaseAllocator* allocator,
+                    bool is_free_buffer_after_fwd);
+    template<typename T>
+    void allocForForward(LLaMAAttentionDynParams params);
+    void free();
+    void forward(TensorMap& inputs, TensorMap& outputs, LLaMAattentionWeights<T>& weights);
+};
