4fa95211914fe2b7f760c475ac9071ee4650ca69
iLeGend <824040212@qq.com>
2023-11-22 13:47:08 +0800

    fix

--- src/kernels/input_embedding.cu
+++ src/kernels/input_embedding.cu
@@ -2,10 +2,10 @@
 #include <cuda.h>
 #include <iostream>
 //#include <utils/gpu_config.h>
-#include "src/kernels/beamsearch_topK.h"
-#include <cub/cub.cuh>
+#include "src/kernels/input_embedding.h"
+#include "src/utils/tensor.h"
 
-__global__ void embeddingFunctor(const int* ipnut_ids,
+__global__ void embeddingFunctor(const int* input_ids,
                Tensor* output, 
                const Tensor* embed_table, // this->weight["model.embed_tokens.weight"]
                const int sequeue_length,
@@ -13,7 +13,7 @@ __global__ void embeddingFunctor(const int* ipnut_ids,
                const int vocab_size) {
     for (int64_t index = blockIdx.x * blockDim.x + threadIdx.x; index < sequeue_length * hidden_size;
          index += blockDim.x * gridDim.x) {
-        input_id = input_ids[index / hidden_size];
+        int input_id = input_ids[index / hidden_size];
         output[index] = embed_table[input_id * hidden_size + index % hidden_size];
     }
 }
@@ -21,6 +21,6 @@ __global__ void embeddingFunctor(const int* ipnut_ids,
 
 void launchInputEmbedding(const int* input_ids, Tensor* output, Tensor* embed_table, const int sequeue_length, const int hidden_size, const int vocab_size) {
     const int blockSize = 256;
-    const int gridSize = (blockSize + outputSize - 1) / blockSize;
+    const int gridSize = (blockSize + sequeue_length * hidden_size - 1) / blockSize;
     embeddingFunctor<<<gridSize, blockSize>>>(input_ids, output, embed_table, sequeue_length, hidden_size, vocab_size);
 }
